ba9fa23936f35ab8a87e9c64dc4b1eb22f3bdc91
JuMaxi <jmaximovski@gmail.com>
2025-06-25 16:53:10 +0100

    Fix charts row height

--- SmartTray/www/assets/js/script.js
+++ SmartTray/www/assets/js/script.js
@@ -7,81 +7,83 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Function to generate gauge chart
     function renderGauge(value, text, id) {
-    var chartDom = document.getElementById(id);
-    if (chartDom) {
-        var myChart = echarts.init(chartDom);
-        var option = {
-            title: {
-                text: text,
-                left: 'center',
-                top: -5,
-                textStyle: {
-                    color: '#fff',
-                    fontSize: 18
-                }
-            },
-            series: [
-                {
-                    type: 'gauge',
-                    startAngle: 200,
-                    endAngle: -20,
-                    min: 0,
-                    max: 40,
-                    splitNumber: 4,
-                    axisLine: {
-                        lineStyle: {
-                            width: 6,
-                            color: [
-                                [0.5, '#91cc75'],
-                                [0.75, '#fac858'],
-                                [1, '#ee6666']
-                            ]
-                        }
-                    },
-                    pointer: {
-                        itemStyle: {
-                            color: '#fff'
-                        }
-                    },
-                    axisTick: {
-                        distance: -15,
-                        length: 4,
-                        lineStyle: {
-                            color: '#fff',
-                            width: 1
-                        }
-                    },
-                    splitLine: {
-                        distance: -20,
-                        length: 8,
-                        lineStyle: {
-                            color: '#fff',
-                            width: 2
-                        }
-                    },
-                    axisLabel: {
+        var chartDom = document.getElementById(id);
+        if (chartDom) {
+            var myChart = echarts.init(chartDom);
+            var option = {
+                title: {
+                    text: text,
+                    left: 'center',
+                    top: 10,
+                    textStyle: {
                         color: '#fff',
-                        distance: -8,
-                        fontSize: 9
-                    },
-                    detail: {
-                        valueAnimation: true,
-                        formatter: '{value} °C',
-                        color: '#fff',
-                        fontSize: 20,
-                    },
-                    data: [
-                        {
-                            value: value // Use function parameter
-                        }
-                    ]
-                }
-            ]
-        };
-        myChart.setOption(option);
-        window.addEventListener('resize', function () {
-            myChart.resize();
-        });
+                        fontSize: 18
+                    }
+                },
+                series: [
+                    {
+                        type: 'gauge',
+                        startAngle: 200,
+                        endAngle: -20,
+                        min: 0,
+                        max: 40,
+                        splitNumber: 4,
+                        axisLine: {
+                            lineStyle: {
+                                width: 6,
+                                color: [
+                                    [0.5, '#91cc75'],
+                                    [0.75, '#fac858'],
+                                    [1, '#ee6666']
+                                ]
+                            }
+                        },
+                        pointer: {
+                            itemStyle: {
+                                color: '#fff'
+                            }
+                        },
+                        axisTick: {
+                            distance: -15,
+                            length: 4,
+                            lineStyle: {
+                                color: '#fff',
+                                width: 1
+                            }
+                        },
+                        splitLine: {
+                            distance: -20,
+                            length: 8,
+                            lineStyle: {
+                                color: '#fff',
+                                width: 2
+                            }
+                        },
+                        axisLabel: {
+                            color: '#fff',
+                            distance: -12,
+                            fontSize: 9
+                        },
+                        detail: {
+                            valueAnimation: true,
+                            formatter: '{value} °C',
+                            color: '#fff',
+                            fontSize: 20,
+                        },
+                        data: [
+                            {
+                                value: value // Use function parameter
+                            }
+                        ]
+                    }
+                ]
+            };
+            myChart.setOption(option);
+            window.addEventListener('resize', function () {
+                myChart.resize();
+            });
+        }
     }
-    }
+
+
 });
