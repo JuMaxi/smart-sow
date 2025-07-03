4d126ee5db955dc2659be5a3075323754036c75e
JuMaxi <jmaximovski@gmail.com>
2025-07-03 13:35:38 +0100

    Add mapping to tray entity

---
+++ SmartTray/SmartTray.Infra/Mappings/TrayMapping.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartTray.Domain.Models;
+
+namespace SmartTray.Infra.Mappings
+{
+    public class TrayMapping : IEntityTypeConfiguration<Tray>
+    {
+        public void Configure(EntityTypeBuilder<Tray> builder)
+        {
+            builder.ToTable("tray");
+            builder.HasKey(k => k.Id);
+            builder.HasOne(u => u.User).WithMany().OnDelete(DeleteBehavior.NoAction);
+            builder.Property(n => n.Name).IsRequired().HasMaxLength(50).HasColumnName("name");
+            builder.Property(c => c.CropType).IsRequired().HasMaxLength(50).HasColumnName("crop_type");
+            builder.Property(s => s.SowingDate).IsRequired().HasColumnName("sowing_date");
+            builder.Property(st => st.Status).IsRequired().HasColumnName("status").HasDefaultValue(Status.Active);
+        }
+    }
+}
