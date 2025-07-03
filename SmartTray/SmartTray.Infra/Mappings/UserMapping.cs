a66c3412c05ec808be2dcb6d74d5e823f6012513
JuMaxi <jmaximovski@gmail.com>
2025-07-03 11:28:42 +0100

    Add mapping to user entity

---
+++ SmartTray/SmartTray.Infra/Mappings/UserMapping.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartTray.Domain.Models;
+
+namespace SmartTray.Infra.Mappings
+{
+    public class UserMapping : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.HasKey(k => k.Id);
+            builder.Property(n => n.Name).IsRequired().HasMaxLength(50).HasColumnName("name");
+            builder.Property(e => e.Email).IsRequired().HasMaxLength(80).HasColumnName("email");
+            builder.Property(p => p.PasswordHash).IsRequired().HasMaxLength(100).HasColumnName("password_hash");
+            builder.Property(s => s.Salt).IsRequired().HasMaxLength(10).HasColumnName("salt");
+            builder.Property(p => p.Postcode).IsRequired().HasMaxLength(10).HasColumnName("post_code");
+        }
+    }
+}
