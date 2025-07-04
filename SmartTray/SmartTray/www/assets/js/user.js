aac35222ec6b8dffb01ff49dc6529f0226c22eed
JuMaxi <jmaximovski@gmail.com>
2025-07-04 16:09:07 +0100

    Add js function to call endpoint to the user login and Add function to display toast message returning from the backend when the login or register doesn't work

--- SmartTray/SmartTray/www/assets/js/user.js
+++ SmartTray/SmartTray/www/assets/js/user.js
@@ -75,24 +75,67 @@ document.addEventListener('DOMContentLoaded', function () {
                 body: JSON.stringify(data) // Convert JS object to JSON string
             });
 
+            // If login fail, display error message
             if (!response.ok) {
-                // TODO: Show an error message to the user
+                showToast(await response.text());
                 return;
-                //throw new Error(`HTTP error! status: ${response.status}`);
             }
 
-            //const result = await response.json(); // Parse JSON response
-            //console.log('Success:', result);
-            // TODO: Redirect to /my-trays.html
-            //return result;
+            // If login is successful, redirect to my trays page
+            window.location.href = "my-trays.html";
         } catch (error) {
-            // TODO: Show an error message to the user
+            //Show an error message to the user
+            showToast("Unexpected error. Please try again.");
             console.error('Error:', error);
         }
     }
 
-    document.getElementById("registerForm").addEventListener("submit", async function(event) {
+    document.getElementById("register-form").addEventListener("submit", async function(event) {
         event.preventDefault();  
         await register();
     });
+
+    // Function to the call the endpoint to login
+    async function login() {
+        let data = {
+            email: document.getElementById("loginEmail").value,
+            password: document.getElementById("loginPassword").value
+        }
+
+        try {
+            const response = await fetch("/User/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            });
+
+            // If login fail, display error message
+            if (!response.ok) {
+                showToast(await response.text());
+                return;
+            }
+
+            // If login is successful, redirect to my trays page
+            window.location.href = "my-trays.html";
+        } catch (error) {
+            showToast("Unexpected error. Please try again.");
+            console.error("Error", error);
+        }
+    }
+
+    document.getElementById("login-form").addEventListener("submit", async function(event) {
+        event.preventDefault();  
+        await login();
+    });
+
+    // Function to show a toast message to the user (when things doesn't work properly)
+    function showToast(message) {
+        const toastBody = document.getElementById('toast-body');
+        toastBody.textContent = message;
+        const toastEl = document.getElementById('liveToast');
+        const toast = new bootstrap.Toast(toastEl);
+        toast.show();
+    }
 });
